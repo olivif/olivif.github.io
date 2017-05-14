45b9b557e232ff1bfba2e0a51638befbe4839c37
Olivia <olivia_git@outlook.com>
2017-05-14 02:34:35 +0200

    Adding blog.

---
+++ _posts/2017-04-26-second-post.markdown
@@ -0,0 +1,8 @@
+---
+title:  "Second post!"
+date:   2017-04-14 15:04:23
+categories: []
+tags: []
+---
+
+This is the second post, here goes nothing!
--- js/main.js
+++ js/main.js
@@ -1,35 +1,14 @@
 ---
-layout: null
+  layout: null
 ---
-$(document).ready(function () {
-  $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '250px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
+  $(document).ready(function () {
+    $('a.blog-button').click(function (e) {
+
+      $('html,body').animate({
+        scrollTop: $(".blog-panel").offset().top
+      },
+        'slow');
+
+
+    })
   })
-
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
-})
